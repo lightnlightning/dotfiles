a31037c5b3b8176ee1ffd78dd453427f151ab56b
lightnlightning <lightning004@outlook.com>
2023-06-21 00:56:02 +0800

    更换会话插件
    旧的会话插件从登陆界面进来无法加载进来，需要手动切换进去

--- nvim/lua/plugins/alpha.lua
+++ nvim/lua/plugins/alpha.lua
@@ -1,10 +1,10 @@
 return {
-  "goolord/alpha-nvim",
+    "goolord/alpha-nvim",
     dependencies = "nvim-neo-tree/neo-tree.nvim",
-  event = "VimEnter",
-  opts = function()
-    local dashboard = require("alpha.themes.dashboard")
-    local logo = [[
+    event = "VimEnter",
+    opts = function()
+        local dashboard = require("alpha.themes.dashboard")
+        local logo = [[
          ██╗      █████╗ ███████╗██╗   ██╗██╗   ██╗██╗███╗   ███╗          Z
          ██║     ██╔══██╗╚══███╔╝╚██╗ ██╔╝██║   ██║██║████╗ ████║      Z    
          ██║     ███████║  ███╔╝  ╚████╔╝ ██║   ██║██║██╔████╔██║   z       
@@ -13,49 +13,50 @@ return {
          ╚══════╝╚═╝  ╚═╝╚══════╝   ╚═╝     ╚═══╝  ╚═╝╚═╝     ╚═╝
     ]]
 
-    dashboard.section.header.val = vim.split(logo, "\n")
-    dashboard.section.buttons.val = {
-      dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
-      dashboard.button("n", " " .. " New file", ":ene <BAR> startinsert <CR>"),
-      dashboard.button("r", " " .. " Recent files", ":Telescope oldfiles <CR>"),
-      dashboard.button("g", " " .. " Find text", ":Telescope live_grep <CR>"),
-      dashboard.button("c", " " .. " Config", ":e $MYVIMRC <CR>"),
-      dashboard.button("s", " " .. " Restore Session", [[:lua require("persistence").load() <cr>:blast<cr>:NeoTreeReveal<cr>]]),
-      dashboard.button("l", "󰒲 " .. " Lazy", ":Lazy<CR>"),
-      dashboard.button("q", " " .. " Quit", ":qa<CR>"),
-    }
-    for _, button in ipairs(dashboard.section.buttons.val) do
-      button.opts.hl = "AlphaButtons"
-      button.opts.hl_shortcut = "AlphaShortcut"
-    end
-    dashboard.section.header.opts.hl = "AlphaHeader"
-    dashboard.section.buttons.opts.hl = "AlphaButtons"
-    dashboard.section.footer.opts.hl = "AlphaFooter"
-    dashboard.opts.layout[1].val = 8
-    return dashboard
-  end,
-  config = function(_, dashboard)
-    -- close Lazy and re-open when the dashboard is ready
-    if vim.o.filetype == "lazy" then
-      vim.cmd.close()
-      vim.api.nvim_create_autocmd("User", {
-        pattern = "AlphaReady",
-        callback = function()
-          require("lazy").show()
-        end,
-      })
-    end
+        dashboard.section.header.val = vim.split(logo, "\n")
+        dashboard.section.buttons.val = {
+            dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
+            dashboard.button("n", " " .. " New file", ":ene <BAR> startinsert <CR>"),
+            dashboard.button("r", " " .. " Recent files", ":Telescope oldfiles <CR>"),
+            dashboard.button("g", " " .. " Find text", ":Telescope live_grep <CR>"),
+            dashboard.button("c", " " .. " Config", ":e $MYVIMRC <CR>"),
+            dashboard.button("s", " " .. " Restore Session", ":SessionManager load_last_session<cr>"),
+            -- dashboard.button("s", " " .. " Restore Session", ":SessionManager load_last_session<cr>:NeoTreeReveal<cr>"),
+            dashboard.button("l", "󰒲 " .. " lazy", ":lazy<cr>"),
+            dashboard.button("q", " " .. " quit", ":qa<cr>"),
+        }
+        for _, button in ipairs(dashboard.section.buttons.val) do
+            button.opts.hl = "alphabuttons"
+            button.opts.hl_shortcut = "alphashortcut"
+        end
+        dashboard.section.header.opts.hl = "alphaheader"
+        dashboard.section.buttons.opts.hl = "alphabuttons"
+        dashboard.section.footer.opts.hl = "alphafooter"
+        dashboard.opts.layout[1].val = 8
+        return dashboard
+    end,
+    config = function(_, dashboard)
+        -- close lazy and re-open when the dashboard is ready
+        if vim.o.filetype == "lazy" then
+            vim.cmd.close()
+            vim.api.nvim_create_autocmd("user", {
+                pattern = "alphaready",
+                callback = function()
+                    require("lazy").show()
+                end,
+            })
+        end
 
-    require("alpha").setup(dashboard.opts)
+        require("alpha").setup(dashboard.opts)
 
-    vim.api.nvim_create_autocmd("User", {
-      pattern = "LazyVimStarted",
-      callback = function()
-        local stats = require("lazy").stats()
-        local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
-        dashboard.section.footer.val = "⚡ Neovim loaded " .. stats.count .. " plugins in " .. ms .. "ms"
-        pcall(vim.cmd.AlphaRedraw)
-      end,
-    })
-  end,
+        vim.api.nvim_create_autocmd("user", {
+            pattern = "lazyvimstarted",
+            callback = function()
+                local stats = require("lazy").stats()
+                local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+                dashboard.section.footer.val = "⚡ neovim loaded " .. stats.count .. " plugins in " .. ms .. "ms"
+                pcall(vim.cmd.alpharedraw)
+            end,
+        })
+    end,
 }
