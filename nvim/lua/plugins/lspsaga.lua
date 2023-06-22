7ea61e71431b2d152c6aeed481a8c37c74fcdce0
lightnlightning <lightning004@outlook.com>
2023-06-23 05:55:26 +0800

    lspsaga按q退出预览

--- nvim/lua/plugins/lspsaga.lua
+++ nvim/lua/plugins/lspsaga.lua
@@ -2,8 +2,47 @@ return{
     "glepnir/lspsaga.nvim",
     event = "LspAttach",
     config = function()
-        require("lspsaga").setup({})
-    end,
+        require("lspsaga").setup({
+            preview = {
+                lines_above = 0,
+                lines_below = 10,
+            },
+            scroll_preview = {
+                scroll_down = "<C-f>",
+                scroll_up = "<C-b>",
+            },
+            request_timeout = 3500,
+
+            -- See Customizing Lspsaga's Appearance
+            ui = {
+                keys = {
+                    quit = { "q", "<ESC>" },
+                },
+            },
+
+            -- For default options for each command, see below
+            finder = {
+                max_height = 0.5,
+                min_width = 30,
+                force_max_height = false,
+                keys = {
+                    -- <C-t> jump back
+                    expand_or_jump = "<CR>",
+                    vsplit = "s",
+                    split = "i",
+                    quit = { "q", "<ESC>" },
+                    close_in_preview = "<ESC>",
+                },
+            },
+            -- code_action = { ... },
+            rename = { quit = "<ESC>", exec = "<CR>" },
+            -- etc.
+            outline = {
+                keys = {
+                    quit = { "q", "<ESC>" },
+                },
+            },
+        })    end,
     dependencies = {
         {"nvim-tree/nvim-web-devicons"},
         --Please make sure you install markdown and markdown_inline parser
