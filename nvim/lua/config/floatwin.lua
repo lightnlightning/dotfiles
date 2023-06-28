7e45a0e626019d3d24a867414a4c033be5d9ffaf
lightnlightning <lightning004@outlook.com>
2023-06-29 00:13:59 +0800

    添加边框

--- nvim/lua/config/floatwin.lua
+++ nvim/lua/config/floatwin.lua
@@ -1,27 +1,40 @@
 local M = {}
 
+local opts = {
+    noremap = true, -- non-recursive
+    silent = true,  -- do not show message
+}
+
+M.opts = {
+    trans_opts = "",
+    line_n = 1
+}
+
 function M.get_words()
     local words = ''
     if vim.fn.mode() == 'n' then
         words = vim.fn.expand("<cword>")
+        M.opts.line_n = 1
     elseif vim.fn.mode() == 'v' or vim.fn.mode() == 'V' then
         vim.cmd([[
             execute 'normal! "9y'
         ]])
         words = vim.fn.getreg('9')
-        words = string.gsub(words, "\n", " ")
+        words, M.opts.line_n = string.gsub(words, "\n", " ")
+        words = string.gsub(words, '"', "'")
+        words = string.gsub(words, '`', "'")
     else
         words = ''
     end
     return words
 end
 
-function M.trans(islive)
+function M.trans()
     local words = '"' .. M.get_words() .. '"'
     local m = ""
-    if islive == "onlive" then
+    if M.opts.trans_opts == "onlive" then
         m = vim.fn.system("trans -no-ansi -b " .. words)
-    elseif islive == "sp" then
+    elseif M.opts.trans_opts == "sp" then
         m = vim.fn.system("trans -no-ansi -sp -b " .. words)
     else
         m = vim.fn.system("sdcv -e " .. words)
@@ -29,21 +42,37 @@ function M.trans(islive)
     return m
 end
 
-function M.floatwin(islive)
+function M.floatwin(otps)
     local words_table = {}
-    local m = M.trans(islive)
+    M.opts.trans_opts = otps
+    local m = M.trans()
     local f_buf = vim.api.nvim_create_buf(false, true)
     for row in string.gmatch(m, "[^\n]+") do table.insert(words_table, row) end -- 分割行并写入表
     vim.api.nvim_buf_set_lines(f_buf, 0, -1, true, words_table)
-    vim.api.nvim_open_win(f_buf, true,
-        { relative = 'cursor', width = 80, height = 15, col = 0, row = 1, --[[ anchor = 'NW', style = 'minimal' ]] })
+    local win_id = vim.api.nvim_open_win(f_buf, true,
+        {
+            relative = 'cursor',
+            width = 80,
+            height = 15,
+            col = 0,
+            row = M.opts.line_n,
+            border = 'single',
+            anchor = 'NW',
+            style = 'minimal'
+        })
+    vim.api.nvim_win_set_option(win_id, "wrap", true)
+    -- vim.api.nvim_win_set_option(win_id, 'winhighlight',
+    --     'Normal:FloatBorder,FloatBorder:FloatBorder,NormalFloat:NormalFloat')
+    -- vim.api.nvim_win_set_option(win_id, 'FloatBorder', 'Normal:FloatBorder')
+    -- vim.api.nvim_win_set_option(win_id, 'FloatBorder', 'NvimFloatingWindow')
+    local win_hl = vim.api.nvim_win_get_option(0, 'winhighlight')
+    vim.api.nvim_win_set_option(win_id, 'winhighlight', win_hl)
+    vim.api.nvim_buf_set_keymap(f_buf, 'n', 'q', ":close<CR>", opts)
 end
 
-local opts = {
-    noremap = true, -- non-recursive
-    silent = true,  -- do not show message
-}
-
+-- border = {
+--   style = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+-- },
 -- vim.api.nvim_create_user_command("FloatwinOnlive", function() M.floatwin("onlive") end, {})
 -- vim.api.nvim_create_user_command("Floatwin", function() M.floatwin(" ") end, {})
 
