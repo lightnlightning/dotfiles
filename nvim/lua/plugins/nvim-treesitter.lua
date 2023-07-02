3e968cf66a04bd5e39468be579b8240c1648e1e2
lightnlightning <lightning004@outlook.com>
2023-07-02 23:41:49 +0800

    扩充选择配置treesitter

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -4,10 +4,21 @@ return {
         require("nvim-treesitter.install").update({ with_sync = true })
     end,
     config = function()
-        require'nvim-treesitter.configs'.setup {
-            ensure_installed = { "vim","bash","c","cpp","javascript","json","lua","python","rust","solidity","html","css","markdown","markdown_inline" },
+        require 'nvim-treesitter.configs'.setup {
+            ensure_installed = { "vim", "bash", "c", "cpp", "javascript", "json", "lua", "python", "rust", "solidity",
+                "html", "css", "markdown", "markdown_inline" },
             highlight = { enable = true },
             indent = { enable = true },
+            incremental_selection = {
+                enable = true,
+                keymaps = {
+                    init_selection = "<CR>", -- set to `false` to disable one of the mappings
+                    node_incremental = "<CR>",
+                    scope_incremental = "<TAB>",
+                    node_decremental = "<BS>",
+                },
+            },
+
         }
     end,
 }
