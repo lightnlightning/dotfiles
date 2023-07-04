68cdc5083cbd191ce57fb9b591a26a2209051cba
lightnlightning <lightning004@outlook.com>
2023-07-05 03:33:05 +0800

    输入模式hjkl

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -15,6 +15,11 @@ vim.keymap.set('n', '<C-j>', ':<C-U>TmuxNavigateDown<cr>', opts)
 vim.keymap.set('n', '<C-k>', ':<C-U>TmuxNavigateUp<cr>', opts)
 vim.keymap.set('n', '<C-l>', ':<C-U>TmuxNavigateRight<cr>', opts)
 vim.keymap.set('n', '<C-w>', ':<C-U>TmuxNavigatePrevious<cr>', opts)
+vim.keymap.set('i', '<C-h>', '<Left>', opts)
+vim.keymap.set('i', '<C-j>', '<Down>', opts)
+vim.keymap.set('i', '<C-k>', '<Up>', opts)
+vim.keymap.set('i', '<C-l>', '<Right>', opts)
+
 -- let g:tmux_navigator_no_mappings = 1
 --
 -- noremap <silent> {Left-Mapping} :<C-U>TmuxNavigateLeft<cr>
