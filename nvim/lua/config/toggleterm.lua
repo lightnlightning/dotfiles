7fdc9912ee447ef51f9ac3fde980646ff84816c3
lightnlightning <lightning004@outlook.com>
2023-07-05 03:53:38 +0800

    toggleterm下的映射

--- nvim/lua/config/toggleterm.lua
+++ nvim/lua/config/toggleterm.lua
@@ -1,4 +1,4 @@
-local T_git  = require('toggleterm.terminal').Terminal
+local T_git   = require('toggleterm.terminal').Terminal
 local git_opt = T_git:new({
     cmd = 'lazygit',
     direction = 'float'
@@ -8,11 +8,11 @@ function git_toggle()
     git_opt:toggle()
 end
 
-vim.api.nvim_set_keymap("n", "<c-g>", "<Cmd>lua git_toggle()<CR>", {noremap = true, silent = true})
-vim.api.nvim_set_keymap("t", "<c-g>", "<Cmd>lua git_toggle()<CR>", {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "<c-g>", "<Cmd>lua git_toggle()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("t", "<c-g>", "<Cmd>lua git_toggle()<CR>", { noremap = true, silent = true })
 
 
-local T_ter  = require('toggleterm.terminal').Terminal
+local T_ter   = require('toggleterm.terminal').Terminal
 local ter_opt = T_ter:new({
     -- cmd = '',
     direction = 'float'
@@ -22,7 +22,13 @@ function ter_toggle()
     ter_opt:toggle()
 end
 
-vim.api.nvim_set_keymap("n", "<c-t>", "<Cmd>lua ter_toggle()<CR>", {noremap = true, silent = true})
-vim.api.nvim_set_keymap("t", "<c-t>", "<Cmd>lua ter_toggle()<CR>", {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "<c-t>", "<Cmd>lua ter_toggle()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("t", "<c-t>", "<Cmd>lua ter_toggle()<CR>", { noremap = true, silent = true })
 
+function _G.set_terminal_keymaps()
+    local opts = { buffer = 0 }
+    vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+end
 
+-- if you only want these mappings for toggle term use term://*toggleterm#* instead
+vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
