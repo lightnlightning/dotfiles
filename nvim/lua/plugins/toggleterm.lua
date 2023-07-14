518d5dd8f7c84907f691cadf1d30f4b9700772fb
lightnlightning <lightning004@outlook.com>
2023-07-14 13:32:06 +0800

    修改宽度

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -1,7 +1,8 @@
 return {
-    'akinsho/toggleterm.nvim', version = "*",
+    'akinsho/toggleterm.nvim',
+    version = "*",
     config = function()
-        require('toggleterm').setup{
+        require('toggleterm').setup {
             -- open_mapping = [[<c-g>]],
             -- 打开新终端后自动进入插入模式
             start_in_insert = true,
@@ -10,10 +11,10 @@ return {
             float_opts = {
                 -- 'single' | 'double' | 'shadow' | 'curved' | ... other options supported by win open
                 border = 'curved', -- 边框
-                width = 150,
+                width = 200,
                 -- height = 100,
-                -- winblend = 1,   -- 透明度
-                zindex = 30,
+                -- winblend = 1, -- 透明度
+                -- zindex = 80,
             },
         }
     end,
